0293a2bd4a8728fd244f4c66a96a031b228662d4
KLoVi <karollvlv@gmail.com>
2024-07-11 20:04:48 +0200

    last_version

--- Changes_after_loadingData.sql
+++
@@ -1,9 +0,0 @@
-USE sales_supermarket;
-
-ALTER TABLE supermarket_sales
-DROP COLUMN supermarket_salescol;
-
-select * from supermarket_sales;
-
-ALTER TABLE supermarket_sales
-MODIFY COLUMN invoice_id VARCHAR(45);
